138a18cbc87903e78f970514297e2e614f0d0865
Leonardo Rodrigues <leonardoacrg@gmail.com>
2023-04-13 20:38:31 -0300

    Fixed Login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { GiExitDoor } from "react-icons/gi";
 import { AiFillCaretDown } from "react-icons/ai";
+import { button } from "@material-tailwind/react";
 
 const Login = () => {
   return (
-    <form className="mx-2 mb-4 w-full border border-l-0 border-t-0 border-slate-400 bg-white pb-8 shadow-md sm:w-1/2 md:w-1/4">
+    <form
+      className="mx-2 mb-32 w-full border border-l-0
+     border-t-0 border-slate-400 bg-white pb-8 shadow-md sm:w-1/2 md:w-1/4"
+    >
       <div className="relative h-full w-full bg-weg-blue">
-        <h2 className="p-4 font-bold">Acesso ao Dashboards WEG</h2>
+        <h2 className="p-4 text-lg font-bold">Acesso ao Dashboards WEG</h2>
         <AiFillCaretDown
           className="absolute top-12 left-[40px] mb-4 -translate-x-1/2 transform text-weg-blue"
-          size={24}
+          size={30}
         />
       </div>
       <div className="px-8 ">
@@ -21,14 +25,15 @@ const Login = () => {
             Nome de Usuário *
           </label>
           <input
-            className="focus:shadow-outline w-full appearance-none rounded-[2px] border
-           border-gray-300 py-1.5 px-3 leading-tight text-gray-700 focus:outline-none"
+            className="focus:shadow-outline w-full appearance-none rounded-[2px] border border-gray-300 py-2
+             px-3 leading-tight text-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500"
             id="username"
             type="text"
             placeholder=""
           />
         </div>
-        <div className="mb-6">
+
+        <div className="mb-4">
           <label
             className="mb-2 block text-sm font-bold text-gray-700"
             htmlFor="password"
@@ -36,32 +41,36 @@ const Login = () => {
             Senha *
           </label>
           <input
-            className="focus:shadow-outline w-full appearance-none rounded-[2px] border border-gray-300 
-          py-1.5 px-3 leading-tight text-gray-700  focus:outline-none"
+            className="focus:shadow-outline w-full appearance-none rounded-[2px] border border-gray-300 py-2 
+          px-3 leading-tight text-gray-700 focus:border-blue-500 focus:outline-none focus:ring-1"
             id="password"
             type="password"
             placeholder=""
           />
         </div>
         <div className="flex items-center">
-          <div className="flex-1">
-            <p className="text-sm text-weg-blue">Esqueceu sua senha?</p>
+          <div className="mb-3 flex-1">
+            <a
+              href="#"
+              className="text-md cursor-pointer text-weg-blue hover:text-sky-900 hover:underline"
+            >
+              Esqueceu sua senha?
+            </a>
           </div>
-          <div className="flex items-center">
-            <div className="flex h-8 w-7 items-center justify-center bg-sky-700">
-              <GiExitDoor className="" />
+          <button className="flex items-center" type="button">
+            <div className="flex h-9 w-8 items-center justify-center rounded-l-[4px] bg-sky-700">
+              <GiExitDoor />
             </div>
 
             <div className="flex flex-col justify-center">
-              <button
-                className="focus:shadow-outline rounded-r-[4px] bg-weg-blue py-1.5 px-4
+              <div
+                className="focus:shadow-outline rounded-r-[4px] bg-weg-blue py-2 px-4
                 text-sm font-normal text-white hover:bg-sky-700 focus:outline-none"
-                type="button"
               >
                 ENTRAR
-              </button>
+              </div>
             </div>
-          </div>
+          </button>
         </div>
         <div className="py-6">
           <hr className="border-t border-gray-300" />
@@ -71,10 +80,13 @@ const Login = () => {
             Novo Cliente?
           </h2>
         </div>
-        <div className="flex w-full items-center pt-4">
-          <h2 className="text-md w-full text-center  text-weg-blue">
+        <div className="flex w-full items-center justify-center pt-4 ">
+          <a
+            href="#"
+            className="text-md cursor-pointer text-center text-weg-blue hover:text-sky-900 hover:underline"
+          >
             CRIAR CONTA
-          </h2>
+          </a>
         </div>
       </div>
     </form>
